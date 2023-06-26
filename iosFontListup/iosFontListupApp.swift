8b34bcd6c57fef7be31bec2a010c26fd8692425d
Makoto Okabe <developer@amakusaweb.com>
2023-06-26 17:31:31 +0900

    Initial Commit

---
+++ iosFontListup/iosFontListupApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosFontListupApp.swift
+//  iosFontListup
+//
+//  Created by MakotoOkabe on 2023/06/26.
+//
+
+import SwiftUI
+
+@main
+struct iosFontListupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
